a815dfe3041c3c38c0020a43b73c8e4f86abc229
abderrahman504 <rahmankhsherb@gmail.com>
2024-08-02 14:01:53 +0300

    Added line duplication from vscode

--- lua/abdelrahman/remap.lua
+++ lua/abdelrahman/remap.lua
@@ -1,6 +1,6 @@
 
 
--- Remap simple movement to a WASD-like configuration
+-- Remap simple movement to a WASD-like layout
 vim.keymap.set('n', 'i', 'k')
 vim.keymap.set('n', 'k', 'j')
 vim.keymap.set('n', 'j', 'h')
@@ -31,13 +31,21 @@ vim.keymap.set('n', ' q', vim.cmd.close)
 vim.keymap.set('n', ' ch', vim.cmd.nohls)
 
 -- VSCode sugar
-vim.keymap.set("n", "<A-k>", ":m .+1<CR>==") -- move line up(n)
-vim.keymap.set("n", "<A-i>", ":m .-2<CR>==") -- move line down(n)
-vim.keymap.set("v", "<A-k>", ":m '>+1<CR>gv=gv") -- move line up(v)
-vim.keymap.set("v", "<A-i>", ":m '<-2<CR>gv=gv") -- move line down(v)
+
+-- Move lines up and down
+vim.keymap.set("n", "<A-k>", ":m .+1<CR>==") -- move line down(n)
+vim.keymap.set("n", "<A-i>", ":m .-2<CR>==") -- move line up(n)
+vim.keymap.set("v", "<A-k>", ":m '>+1<CR>gv=gv") -- move line down(v)
+vim.keymap.set("v", "<A-i>", ":m '<-2<CR>gv=gv") -- move line up(v)
+
+-- Duplicate lines up and down
+vim.keymap.set("n", "<A-S-k>", "yyp") -- duplicate line down(n)
+vim.keymap.set("n", "<A-S-i>", "yyP") -- duplicate line up(n)
+vim.keymap.set("v", "<A-S-k>", "yPgv") -- duplicate lines down(n)
+vim.keymap.set("v", "<A-S-i>", "ygv<Esc>pgv") -- duplicate lines up(n)
 
 
-
+-- Toggle comments on lines
 
 
 
