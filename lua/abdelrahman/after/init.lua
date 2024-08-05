af5ad43a235bf2193e20ad9839725e49c639210f
abderrahman504 <rahmankhsherb@gmail.com>
2024-08-05 16:03:54 +0300

    Added LSP-Zero plugin

--- lua/abdelrahman/after/init.lua
+++ lua/abdelrahman/after/init.lua
@@ -1,4 +1,4 @@
 require('abdelrahman.after.treesitter')
 require('abdelrahman.after.telescope')
 require('abdelrahman.after.colorscheme')
-
+require('abdelrahman.after.lsp-zero')
--- lua/abdelrahman/plugins/init.lua
+++ lua/abdelrahman/plugins/init.lua
@@ -10,5 +10,16 @@ return {
         opts = {},
     },
 
-    { 'nvim-treesitter/nvim-treesitter', build = ':TSUpdate' }
+    { 'nvim-treesitter/nvim-treesitter', build = ':TSUpdate' },
+
+    {'VonHeikemen/lsp-zero.nvim', branch = 'v3.x'},
+    {'hrsh7th/cmp-nvim-lsp'},
+    {'hrsh7th/nvim-cmp'},
+    {'L3MON4D3/LuaSnip'},
+
+    {
+        "williamboman/mason.nvim",
+        "williamboman/mason-lspconfig.nvim",
+        "neovim/nvim-lspconfig",
+    }
 }
