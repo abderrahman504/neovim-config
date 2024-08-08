9e4204a25f5440f032edec5217184e9f3846fda2
abderrahman504 <rahmankhsherb@gmail.com>
2024-08-08 17:02:51 +0300

    Added telescope help search shortcut

--- lua/abdelrahman/after/telescope.lua
+++ lua/abdelrahman/after/telescope.lua
@@ -4,6 +4,7 @@ local builtin = require("telescope.builtin")
 vim.keymap.set('n', '<C-p>', builtin.find_files)
 vim.keymap.set('n', '<C-g>', builtin.live_grep)
 vim.keymap.set('n', '<C-s>', builtin.current_buffer_fuzzy_find)
+vim.keymap.set('n', '<Leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
 
 require("telescope").setup(
 )
